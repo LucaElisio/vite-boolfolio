bb9cbb6cbb91c209c6f6acc3b8870258fae9b0ec
LucaElisio <personal.elix@gmail.com>
2024-06-30 16:07:08 +0200

    feat: axios call

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,22 @@
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+    data() {
+        return {
+            projects: []
+        }
+    },
+
+    created(){
+        axios.get('http://127.0.0.1:8000/api/projects').then((resp) => {
+
+            // console.log(resp);
+
+            // Prelevo i dati e li assegno a projects
+            this.projects = resp.data.result;
+        })
+    }
 }
 </script>
 
