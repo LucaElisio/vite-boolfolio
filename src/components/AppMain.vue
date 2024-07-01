55bdabb85e2fa9b885ec93fe9533cefb310ff3be
LucaElisio <personal.elix@gmail.com>
2024-07-01 16:43:25 +0200

    feat: pagination

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,18 +11,52 @@ export default {
 
     data() {
         return {
-            projects: []
+            projects: [],
+            lastPage: 0,
+            curPage: 1,
         }
     },
 
     created(){
-        axios.get('http://127.0.0.1:8000/api/projects').then((resp) => {
+        this.getProjects()
+    },
 
-            // console.log(resp);
+    methods: {
+
+        getProjects(){
+            axios
+                .get('http://127.0.0.1:8000/api/projects', {
+                    params: {
+                        page: this.curPage,
+                    },
+            })
+                .then((resp) => {
 
             // Prelevo i dati e li assegno a projects
-            this.projects = resp.data.result;
-        })
+            this.projects = resp.data.result.data;
+            this.lastPage = resp.data.result.last_page;
+
+            })
+        },
+
+        changePage(newPage){
+            this.curPage = newPage;
+            this.getProjects();
+        },
+
+        nextPage(){
+            if(this.curPage < this.lastPage) {
+                this.curPage++;
+                this.getProjects(); 
+            }
+        },
+
+        prevPage(){
+            if(this.curPage !== 1){
+                this.curPage--;
+                this.getProjects();
+            }
+        }
     }
 }
 </script>
@@ -32,9 +66,19 @@ export default {
     <div class="container">
         <div class="row">
             <div class="col" v-for="project in this.projects">
-                <ProjectCard :title="project.title" :content="project.content" :startDate="project.start_date" :endDate="project.end_date"/>
+                <ProjectCard class="h-100" :title="project.title" :content="project.content" :startDate="project.start_date" :endDate="project.end_date"/>
             </div>
         </div>
+        <div class="d-flex justify-content-center mt-4">
+            <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="#" :disabled="curPage === 1" @click.prevent="prevPage()" >Previous</a></li>
+                <li class="page-item" v-for="page in lastPage" :class="{'active' : curPage === page}"><a class="page-link" @click.prevent="changePage(page)">{{page}}</a></li>
+                <li class="page-item"><a class="page-link" href="#" :disabled="curPage === lastPage" @click.prevent="nextPage()">Next</a></li>
+            </ul>
+        </nav>
+        </div>
+
     </div>
 </template>
 
